f678059a7d1e759f45e82c9515db2589cc7551a3
Chris Donahue <christopherdonahue@gmail.com>
2017-10-31 14:07:03 -0700

    Train script saves git SHA

--- train.sh
+++ train.sh
@@ -1,5 +1,8 @@
 TRAIN_DIR=./train
 
+git rev-parse HEAD > ${TRAIN_DIR}/git_sha.txt
+cp train.sh ${TRAIN_DIR}
+
 python sdgan.py train ${TRAIN_DIR} \
 	--data_dir ./data/msceleb12k \
 	--data_set msceleb12k \
